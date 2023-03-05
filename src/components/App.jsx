8f42e726cc19d4a0119a6b706a872f997d533540
Nazzik1992 <tkachuknazarii15@gmail.com>
2023-03-05 18:33:56 +0200

    addTask2

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,13 +1,21 @@
 import { Profile } from 'components/Profile/profile';
+import { Statistics } from 'components/Statistics/Statistics'
+import { FriendList } from './FriendList/FriendList'
 import user from 'data/user.json';
+import data from 'data/data.json';
+import friends from 'data/friends.json';
 
 export const App = () => {
-  return (
+  return (<div>
     <Profile
     username={user.username}
     tag={user.tag}
     location={user.location}
     avatar={user.avatar}
     stats={user.stats} />
+
+    <Statistics title stats={data} />
+    <FriendList friends={friends} />
+    </div>
   );
 };
